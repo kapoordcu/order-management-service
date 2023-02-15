a1beb0b3900efeb559c4c00ee3f18c5cf6e731d5
kapoordcu <gaurav.kapoor23@outlook.com>
2023-02-15 15:48:56 +0100

    SKILLS-1548: order service added

--- product-service/src/test/java/com/skills/gk/ProductApplicationTests.java
+++ product-service/src/test/java/com/skills/gk/ProductApplicationTests.java
@@ -1,7 +1,6 @@
 package com.skills.gk;
 
 import com.skills.gk.dto.ProductRequest;
-import com.skills.gk.model.Product;
 import com.skills.gk.repository.ProductRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,7 +11,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -21,7 +19,6 @@ import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.math.BigDecimal;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
